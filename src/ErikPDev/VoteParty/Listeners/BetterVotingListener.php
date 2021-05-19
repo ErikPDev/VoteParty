3d8646388a8838b8e65e63e5a312864ae63ccd75
ErikPDev <33035080+ErikPDev@users.noreply.github.com>
2021-05-19 10:42:05 +0300

    PhpStan Removal
    Removed PHPStan because it's annoying, and saying errors that doesn't make sense.

--- src/ErikPDev/VoteParty/Listeners/BetterVotingListener.php
+++ src/ErikPDev/VoteParty/Listeners/BetterVotingListener.php
@@ -46,7 +46,7 @@ class BetterVotingListener implements Listener{
         $this->plugin = $plugin;
     }
 
-    public function PlayerVoteEvent(PlayerVoteEvent $player) {
+    public function PlayerVoteEvent(PlayerVoteEvent $event) {
         $this->plugin->PlayerVoted();
     }
 }
